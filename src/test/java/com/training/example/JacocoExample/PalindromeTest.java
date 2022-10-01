0dee888189dd5455aafaf80ce0ef84d82c059caa
Debdut Goswami <debdutgoswami@gmail.com>
2022-10-01 23:09:34 +0530

    Update PalindromeTest.java

--- src/test/java/com/training/example/JacocoExample/PalindromeTest.java
+++ src/test/java/com/training/example/JacocoExample/PalindromeTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class PalindromeTest {
 
 	@Test
-	public void whenEmptyString_thenAccept() {
+	public void whenEmptyStringThenAccept() {
 	    Palindrome palindromeTester = new Palindrome();
 	    assertTrue(palindromeTester.isPalindrome("madam"));
 	}
